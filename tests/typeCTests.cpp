f0d90ed7deb871bd31fc75acfff789d29ab991e7
Brian-Uong <uongb03@gmail.com>
2024-03-13 16:20:31 -0700

    Added tests for Type_C

--- tests/typeCTests.cpp
+++ tests/typeCTests.cpp
@@ -1,2 +1,22 @@
 #include "../src/Type_C.cpp"
 #include "gtest/gtest.h"
+
+TEST(TypeCTests, LevelUpTest) {
+  TypeC typeC("Test Player", 10, 5, 0, 0);
+  typeC.levelUp();
+
+  ASSERT_EQ(ss.str(), "Level up!");
+  ASSERT_EQ(typeC.getLevel(), 1);
+  ASSERT_EQ(typeC.getMaxHealth(), 11);
+  ASSERT_EQ(typeC.getHealth(), 11);
+  ASSERT_EQ(typeC.getAttackStrength(), 8);
+}
+
+TEST(TypeCTests, InitialStatsTest) {
+  TypeC typeC("Test Player", 10 ,5, 0, 0);
+
+  ASSERT_EQ(typeC.getLevel(), 0);
+  ASSERT_EQ(typeC.getMaxHealth(), 10);
+  ASSERT_EQ(typeC.getHealth(), 10);
+  ASSERT_EQ(typeC.getAttackStrength(), 5);
+}
