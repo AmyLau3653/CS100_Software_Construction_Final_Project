b8c9c59b7fd1cea9f13ee6d15160a288a87066e1
bryany1220 <byu26169@gmail.com>
2024-03-12 23:05:42 -0700

    add main cpp to cmmit

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@
 #include "header/Type_B.h"
 #include "header/Type_C.h"
 #include "header/gameplay.h"
-#include "Output.h"
-#include "wrongInput.hpp"
+#include "header/Output.h"
+#include "headerwrongInput.hpp"
 
 using namespace std;
 
