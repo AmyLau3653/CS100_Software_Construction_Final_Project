1fbe3e70cb8321da17ef67af4816f4393af95f04
bryany1220 <byu26169@gmail.com>
2024-03-12 23:58:40 -0700

    Header guard position

--- header/Player.h
+++ header/Player.h
@@ -1,10 +1,11 @@
+#ifndef PLAYER_H_
+#define PLAYER_H_
 #include "room.h"
 #include <string>
 #include <vector>
 using namespace std;
 
-#ifndef PLAYER_H_
-#define PLAYER_H_
+
 
 enum PlayerType { TYPE_A, TYPE_B, TYPE_C };
 
