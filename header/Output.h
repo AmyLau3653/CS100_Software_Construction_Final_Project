12c7e06b9e691c6b3677fbd147da4232365f7225
bryany1220 <byu26169@gmail.com>
2024-03-12 21:40:35 -0700

    Add Output.h

--- header/Output.h
+++ header/Output.h
@@ -9,19 +9,19 @@ using namespace std;
 class Output {
 public:
   Output();
-  void OutputDeath(Player);
+  void OutputDeath(Player*);
   void OutputChoice();
   void OutputGameSetup();
-  void OutputMove(Player);
-  void OutputLevelUpPlayer(Player);
-  void OutputAnalyze(Player, Player);
-  void OutputEncounter(Player, Player);
-  void OutputWin(Player);
+  void OutputMove(Player*);
+  void OutputLevelUpPlayer(Player*);
+  void OutputAnalyze(Player*, Player*);
+  void OutputEncounter(Player*, Player*);
+  void OutputWin(Player*);
   void OutputMenu();
   void OutputPlay();
   void OutputHowToPlay();
-  void OutputExitRoom(Player);
-  void OutputFight(Player, Player);
+  void OutputExitRoom(Player*);
+  void OutputFight(Player*, Player*);
   void OutputEnemyIsClose(Player*, Player*);
   void OutputExitIsClose(Player*, int, int); //write new function to immediately access exit
 
