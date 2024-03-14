2629069f678548fbd3899edf3cbe0c757af74ff7
alau030 <alau030@ucr.edu>
2024-03-13 20:42:45 -0700

    Update typeCTests

--- tests/typeCTests.cpp
+++ tests/typeCTests.cpp
@@ -5,19 +5,4 @@
 TEST(TypeCTests, LevelUpTest) {
   Player* p = new TypeC("Test Player", 5, 5, 0, 0);
   p->levelUp();
-
-  // ASSERT_EQ(ss.str(), "Level up!");
-  // ASSERT_EQ(p.getLevel(), 1);
-  // ASSERT_EQ(p.getMaxHealth(), 11);
-  // ASSERT_EQ(p.getHealth(), 11);
-  // ASSERT_EQ(p.getAttackStrength(), 8);
 }
-
-// TEST(TypeCTests, InitialStatsTest) {
-//   Player* p = new Type_C("Test Player", 10, 5, 0, 0);
-
-//   ASSERT_EQ(p.getLevel(), 0);
-//   ASSERT_EQ(p.getMaxHealth(), 10);
-//   ASSERT_EQ(p.getHealth(), 10);
-//   ASSERT_EQ(p.getAttackStrength(), 5);
-// }
