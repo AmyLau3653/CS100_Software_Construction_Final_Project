be579072dd6a38872362ef5c5a623eab48ce033d
bryany1220 <byu26169@gmail.com>
2024-03-13 22:13:10 -0700

    Add Cmakefiles

---
+++ CMakeFiles/runGame.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/csmajs/byu064/final-project-buong001-alau030-byu064-imcke003/main.cpp" "CMakeFiles/runGame.dir/main.cpp.o" "gcc" "CMakeFiles/runGame.dir/main.cpp.o.d"
+  "/home/csmajs/byu064/final-project-buong001-alau030-byu064-imcke003/src/Output.cpp" "CMakeFiles/runGame.dir/src/Output.cpp.o" "gcc" "CMakeFiles/runGame.dir/src/Output.cpp.o.d"
+  "/home/csmajs/byu064/final-project-buong001-alau030-byu064-imcke003/src/Player.cpp" "CMakeFiles/runGame.dir/src/Player.cpp.o" "gcc" "CMakeFiles/runGame.dir/src/Player.cpp.o.d"
+  "/home/csmajs/byu064/final-project-buong001-alau030-byu064-imcke003/src/Type_A.cpp" "CMakeFiles/runGame.dir/src/Type_A.cpp.o" "gcc" "CMakeFiles/runGame.dir/src/Type_A.cpp.o.d"
+  "/home/csmajs/byu064/final-project-buong001-alau030-byu064-imcke003/src/Type_B.cpp" "CMakeFiles/runGame.dir/src/Type_B.cpp.o" "gcc" "CMakeFiles/runGame.dir/src/Type_B.cpp.o.d"
+  "/home/csmajs/byu064/final-project-buong001-alau030-byu064-imcke003/src/Type_C.cpp" "CMakeFiles/runGame.dir/src/Type_C.cpp.o" "gcc" "CMakeFiles/runGame.dir/src/Type_C.cpp.o.d"
+  "/home/csmajs/byu064/final-project-buong001-alau030-byu064-imcke003/src/room.cpp" "CMakeFiles/runGame.dir/src/room.cpp.o" "gcc" "CMakeFiles/runGame.dir/src/room.cpp.o.d"
+  "/home/csmajs/byu064/final-project-buong001-alau030-byu064-imcke003/src/wrongInput.cpp" "CMakeFiles/runGame.dir/src/wrongInput.cpp.o" "gcc" "CMakeFiles/runGame.dir/src/wrongInput.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/runGame.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runGame.dir/main.cpp.o"
+  "CMakeFiles/runGame.dir/main.cpp.o.d"
+  "CMakeFiles/runGame.dir/src/Output.cpp.o"
+  "CMakeFiles/runGame.dir/src/Output.cpp.o.d"
+  "CMakeFiles/runGame.dir/src/Player.cpp.o"
+  "CMakeFiles/runGame.dir/src/Player.cpp.o.d"
+  "CMakeFiles/runGame.dir/src/Type_A.cpp.o"
+  "CMakeFiles/runGame.dir/src/Type_A.cpp.o.d"
+  "CMakeFiles/runGame.dir/src/Type_B.cpp.o"
+  "CMakeFiles/runGame.dir/src/Type_B.cpp.o.d"
+  "CMakeFiles/runGame.dir/src/Type_C.cpp.o"
+  "CMakeFiles/runGame.dir/src/Type_C.cpp.o.d"
+  "CMakeFiles/runGame.dir/src/room.cpp.o"
+  "CMakeFiles/runGame.dir/src/room.cpp.o.d"
+  "CMakeFiles/runGame.dir/src/wrongInput.cpp.o"
+  "CMakeFiles/runGame.dir/src/wrongInput.cpp.o.d"
+  "runGame"
+  "runGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
