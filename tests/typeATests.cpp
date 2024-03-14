a5bd076745e1f8599cb2d40ab4c54dc61953076b
alau030 <alau030@ucr.edu>
2024-03-13 20:40:56 -0700

    Update typeATests file

--- tests/typeATests.cpp
+++ tests/typeATests.cpp
@@ -5,19 +5,4 @@
 TEST(TypeATests, LevelUpTest) {
   Player* p = new TypeA("Test Player", 5, 5, 0, 0);
   p->levelUp();
-
-  // ASSERT_EQ(ss.str(), "Level up!");
-  // ASSERT_EQ(p->getLevel(), 1);
-  // ASSERT_EQ(p.getMaxHealth(), 12);
-  // ASSERT_EQ(p.getHealth(), 12);
-  // ASSERT_EQ(p.getAttackStrength(), 7);
 }
-
-// TEST(TypeATests, InitialStatsTest) {
-//   Player* p = new TypeA("Test Player", 10, 5, 0, 0);
-
-//   ASSERT_EQ(p.getLevel(), 0);
-//   ASSERT_EQ(p.getMaxHealth(), 10);
-//   ASSERT_EQ(p.getHealth(), 10);
-//   ASSERT_EQ(p.getAttackStrength(), 5);
-// }
